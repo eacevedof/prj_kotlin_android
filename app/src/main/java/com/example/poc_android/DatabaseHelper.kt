92bbc9dfff6d409faba94ceac336025ac6abb119
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-15 13:02:47 +0200

    android crud poc by gemini

---
+++ app/src/main/java/com/example/poc_android/DatabaseHelper.kt
@@ -0,0 +1,211 @@
+package com.example.poc_android
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import android.util.Log
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(
+    context, DATABASE_NAME, null, DATABASE_VERSION
+) {
+
+    companion object {
+        private const val DATABASE_NAME = "UserDatabase.db"
+        private const val DATABASE_VERSION = 1
+
+        // Table and Column names
+        private const val TABLE_USERS = "users"
+
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_UUID = "uuid"
+        private const val COLUMN_NAME = "name"
+        private const val COLUMN_BIRTH_DATE = "birth_date"
+        private const val COLUMN_EMAIL = "email"
+        private const val COLUMN_PHONE = "phone"
+        private const val COLUMN_USERNAME = "username"
+        private const val COLUMN_ACCESS_PASSWORD = "access_password"
+
+        private val DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTableQuery =
+            "CREATE TABLE $TABLE_USERS (" +
+                    "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$COLUMN_UUID TEXT UNIQUE, " +
+                    "$COLUMN_NAME TEXT, " +
+                    "$COLUMN_BIRTH_DATE TEXT, " +
+                    "$COLUMN_EMAIL TEXT, " +
+                    "$COLUMN_PHONE TEXT, " +
+                    "$COLUMN_USERNAME TEXT UNIQUE, " +
+                    "$COLUMN_ACCESS_PASSWORD TEXT)"
+        db?.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS $TABLE_USERS")
+        onCreate(db)
+    }
+
+    fun createUser(user: User): Long {
+        val db = this.writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_UUID, user.uuid)
+            put(COLUMN_NAME, user.name)
+            put(COLUMN_BIRTH_DATE, DATE_FORMAT.format(user.birthDate))
+            put(COLUMN_EMAIL, user.email)
+            put(COLUMN_PHONE, user.phone)
+            put(COLUMN_USERNAME, user.username)
+            put(COLUMN_ACCESS_PASSWORD, user.accessPassword)
+        }
+        val newRowId = db.insert(TABLE_USERS, null, values)
+        db.close()
+        return newRowId
+    }
+
+    fun getUserByUuid(uuid: String): User? {
+        val db = this.readableDatabase
+        val cursor = db.query(
+            TABLE_USERS,
+            null, // Todas las columnas
+            "$COLUMN_UUID=?",
+            arrayOf(uuid),
+            null, null, null
+        )
+        if (cursor.count == 0) {
+            cursor.close()
+            return null
+        }
+
+        var user: User? = null
+        if (cursor.moveToFirst()) {
+            user = cursor.getUserFromCursor()
+        }
+        cursor.close()
+        db.close()
+        return user
+    }
+
+    fun getUserByUserId(userId: Long): User? {
+        Log.d("DatabaseHelper", "getUserByUserId user-id: $userId")
+        val db = this.readableDatabase
+        val cursor = db.query(
+            TABLE_USERS,
+            null, // Todas las columnas
+            "$COLUMN_ID=?",
+            arrayOf(userId.toString()),
+            null, null, null
+        )
+
+        if (cursor.count == 0) {
+            cursor.close()
+            Log.d("DatabaseHelper", "getUserByUserId user-id: $userId not found")
+            return null
+        }
+
+        cursor.moveToFirst()
+        //id, uuid, name, birth_date, email, phone, username, access_password
+        val user = User(
+            id = cursor.getLong(cursor.getColumnIndexOrThrow("id")),
+            uuid = cursor.getString(cursor.getColumnIndexOrThrow("uuid")),
+            name = cursor.getString(cursor.getColumnIndexOrThrow("name")),
+            birthDate = Date(cursor.getLong(cursor.getColumnIndexOrThrow("birth_date"))),
+            email = cursor.getString(cursor.getColumnIndexOrThrow("email")),
+            phone = cursor.getString(cursor.getColumnIndexOrThrow("phone")),
+            username = cursor.getString(cursor.getColumnIndexOrThrow("username")),
+            accessPassword = cursor.getString(cursor.getColumnIndexOrThrow("access_password"))
+        )
+
+        cursor.close()
+        Log.d("DatabaseHelper", "user-id: $userId loaded")
+        return user
+    }
+
+    fun getAllUsers(): MutableList<User> {
+        val userList = mutableListOf<User>()
+        val db = this.readableDatabase
+        val cursor = db.query(
+            TABLE_USERS, null,
+            null, null,
+            null, null,
+            null
+        )
+
+        if (cursor.count == 0) {
+            cursor.close()
+            return userList
+        }
+
+        if (cursor.moveToFirst()) {
+            do {
+                userList.add(cursor.getUserFromCursor())
+            }
+            while (cursor.moveToNext())
+        }
+
+        cursor.close()
+        db.close()
+        return userList
+    }
+
+    fun updateUser(user: User): Int {
+        val db = this.writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_UUID, user.uuid)
+            put(COLUMN_NAME, user.name)
+            put(COLUMN_BIRTH_DATE, DATE_FORMAT.format(user.birthDate))
+            put(COLUMN_EMAIL, user.email)
+            put(COLUMN_PHONE, user.phone)
+            put(COLUMN_USERNAME, user.username)
+            put(COLUMN_ACCESS_PASSWORD, user.accessPassword)
+        }
+        val rowsAffected = db.update(
+            TABLE_USERS,
+            values,
+            "$COLUMN_ID=?",
+            arrayOf(user.id.toString())
+        )
+        db.close()
+        return rowsAffected
+    }
+
+    fun deleteUserByUuid(uuid: String): Int {
+        val db = this.writableDatabase
+        val rowsAffected = db.delete(
+            TABLE_USERS,
+            "$COLUMN_UUID=?",
+            arrayOf(uuid)
+        )
+        db.close()
+        return rowsAffected
+    }
+
+    fun deleteUserByUserId(userId: Long): Int {
+        val db = this.writableDatabase
+        val rowsAffected = db.delete(
+            TABLE_USERS,
+            "$COLUMN_ID=?",
+            arrayOf(userId.toString())
+        )
+        db.close()
+        return rowsAffected
+    }
+
+    private fun Cursor.getUserFromCursor(): User {
+        val id = getLong(getColumnIndexOrThrow(COLUMN_ID))
+        val uuid = getString(getColumnIndexOrThrow(COLUMN_UUID))
+        val name = getString(getColumnIndexOrThrow(COLUMN_NAME))
+        val birthDateString = getString(getColumnIndexOrThrow(COLUMN_BIRTH_DATE))
+        val birthDate = DATE_FORMAT.parse(birthDateString) as Date
+        val email = getString(getColumnIndexOrThrow(COLUMN_EMAIL))
+        val phone = getString(getColumnIndexOrThrow(COLUMN_PHONE))
+        val username = getString(getColumnIndexOrThrow(COLUMN_USERNAME))
+        val accessPassword = getString(getColumnIndexOrThrow(COLUMN_ACCESS_PASSWORD))
+        return User(id, uuid, name, birthDate, email, phone, username, accessPassword)
+    }
+}
---
+++ makefile
@@ -0,0 +1,28 @@
+#!/bin/bash
+TODAY := $(shell date +'%Y-%m-%d')
+CURRENT_BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
+
+help: ## Show this help message
+	@echo "usage: make [target]"
+	@echo
+	@echo "targets:"
+	@egrep "^(.+)\:\ ##\ (.+)" ${MAKEFILE_LIST} | column -t -c 2 -s ":#"
+
+gitpush: ## git push m=any message
+	clear;
+	git add .; git commit -m "$(m)"; git push;
+
+gp: ## git push m=any message
+	clear;
+	git add .; git commit -m "$(m)"; git push;
+
+CURRENT_BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
+update-branch:  ## update main branches
+	git fetch --all;
+
+	git checkout main; git reset --hard origin/main;
+	git checkout dev-eaf; git reset --hard origin/dev-eaf;
+
+	git checkout $(CURRENT_BRANCH);
+	git branch --sort=-committerdate
+	git diff dev-eaf main --name-only
