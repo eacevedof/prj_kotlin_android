68977371f4ce7a8fc5f35871c722825d7ffb060a
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-18 21:18:41 +0200

    listado pelis

--- composeApp/src/commonMain/kotlin/io/devexpert/kmpmovies/App.kt
+++ composeApp/src/commonMain/kotlin/io/devexpert/kmpmovies/App.kt
@@ -1,8 +1,16 @@
 package io.devexpert.kmpmovies
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -11,6 +19,16 @@ fun App() {
     //ruta de entrada de compose-mt, todo lo que hagamos aqui sera multiplataforma
     //internamente cada plataforma lo compilará a nativo
     MaterialTheme {
-        Text("\n\n\n\nhello world")
+        //surface permitie configurar los colores de la ui segun dispositivo, tema claro u oscuro
+        Surface (modifier = Modifier.fillMaxSize()) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = 120.dp),
+                contentPadding = PaddingValues(4.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                //aqui vamos a mostrar un listado de peliculas, vamos a psarle un listado de dtos/entities movies
+            }
+        }
     }
 }
